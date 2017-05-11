eb828f6f0ce7f48782e700992bf5c1387a60e961
João Fernandes <ojoaofernandes@gmail.com>
2017-05-11 03:51:36 +0100

    Add connection/end event handler to the client.

--- src/client.js
+++ src/client.js
@@ -12,6 +12,11 @@ client.on('data', function (message) {
     console.log(message.toString());
 });
 
+client.on('end', function () {
+    console.log('Connection closed.');
+    process.exit();
+});
+
 process.stdin.on('readable', function () {
     let message = process.stdin.read();
     if (!message) return;
