9ea0cfaee83ac5eded58c31b0662c4420dbe2cf9
João Fernandes <ojoaofernandes@gmail.com>
2017-05-11 03:53:14 +0100

    Save connections and add broadcast function to the server.

--- src/server.js
+++ src/server.js
@@ -2,12 +2,23 @@
 
 const net = require('net');
 
+const connections = [];
+
+const broadcast = function (sender, message) {
+    connections.forEach(function (connection) {
+        if (sender === connection) return;
+        connection.write(message);
+    });
+};
+
 const server = net.createServer(function (connection) {
-    connection.write('Connection established with the server...');
+    console.log('New client connected...');
+    connections.push(connection);
 
     connection.on('data', function (message) {
-        console.log(message.toString());
+        broadcast(connection, message);
     });
 });
 
 server.listen(3000);
+console.log('Server up listen on port 3000...');
