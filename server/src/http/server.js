9afca1dd6c12067f92a4665b0deb2d8b4cf12884
jolasman <jolasman@hotmail.com>
2017-05-25 21:27:04 +0100

    facebbok login working in http server

--- server/src/http/server.js
+++ server/src/http/server.js
@@ -1,93 +1,102 @@
-'use strict';
+var express = require('express');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
 
-const http = require('http');
-const httpHandler = require('./httpHandler.js');
+// Configure the Facebook strategy for use by Passport.
+//
+// OAuth 2.0-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Facebook API on the user's
+// behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new Strategy({
+    clientID: '1945723129042657',
+    clientSecret: '7669419a08d9efeb323b03f35977187a',
+    callbackURL: 'http://localhost:3000/login/facebook/return'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    console.log("\n************* token **********************************\n\n");
+    console.log(accessToken);
+    console.log("\n*************  profile *************************************\n");
+    console.log(profile);
+    console.log("\n*************  name *************************************\n");
+    console.log(profile.displayName);
+    console.log("\n*************  isd *************************************\n");
+    console.log(profile.id);
+    console.log("\n******************************************************\n\n");
 
-
-// import our modules 
-var Router       = require('router')
-var finalhandler = require('finalhandler')
-var compression  = require('compression')
-var bodyParser   = require('body-parser')
-var passport = require('passport-facebook')
-var passportStrategy = require('passport-strategy')
-
-
-/*
-const httpServer = http.createServer(httpHandler);
-
-const server = {
-    run(options) {
-        const callback = function () {
-            console.log('HTTP Server is up and listening on'
-                , options.host + ':' + options.port
-                , '\nCTRL+C to stop'
-            );
-        };
-
-        httpServer.listen(options, callback);
-    }
-};
-
-module.exports = server;*/
-
-
- 
-// store our message to display 
-var message = "Hello World!"
- 
-// initialize the router & server and add a final callback. 
-var router = Router()
-var server = http.createServer(function onRequest(req, res) {
-  router(req, res, finalhandler(req, res))
-})
- 
-// use some middleware and compress all outgoing responses 
-router.use(compression())
- 
-// handle `GET` requests to `/message` 
-router.get('/message', function (req, res) {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-  res.end(message + '\n')
-})
-
-// Redirect the user to Facebook for authentication.  When complete,
-// Facebook will redirect the user back to the application at
-//     /auth/facebook/callback
-router.get('/auth/facebook', passport.authenticate('facebook'));
-
-// Facebook will redirect the user to this URL after approval.  Finish the
-// authentication process by attempting to obtain an access token.  If
-// access was granted, the user will be logged in.  Otherwise,
-// authentication has failed.
-router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/',
-                                      failureRedirect: '/login' }));
- 
+    return cb(null, profile);
+  }));
 
 
-// create and mount a new router for our API 
-var api = Router()
-router.use('/api/', api)
- 
-// add a body parsing middleware to our API 
-api.use(bodyParser.json())
- 
-// handle `PATCH` requests to `/api/set-message` 
-api.patch('/set-message', function (req, res) {
-  if (req.body.value) {
-    message = req.body.value
- 
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    res.end(message + '\n')
-  } else {
-    res.statusCode = 400
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    res.end('Invalid API Syntax\n')
-  }
-})
- 
-// make our http server listen to connections 
-server.listen(8080)
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+// Create a new Express application.
+var app = express();
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Define routes.
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/return', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+app.listen(3000);
+
