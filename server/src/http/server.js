0e71b37f64d2323511919349f8a91ec09e0c392f
jolasman <jolasman@hotmail.com>
2017-05-26 23:01:27 +0100

    login do google - esta a dar erro quando recebe os dados nao sei porque

--- server/src/http/server.js
+++ server/src/http/server.js
@@ -1,26 +1,15 @@
 var express = require('express');
 var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
+var StrategyFacebook = require('passport-facebook').Strategy;
 var StrategyTwitter = require('passport-twitter').Strategy;
+var StrategyGoogle = require('passport-google-oauth2').Strategy;
 
-// Configure the Facebook strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-passport.use(new Strategy({
+passport.use(new StrategyFacebook({
     clientID: '1945723129042657',
     clientSecret: '7669419a08d9efeb323b03f35977187a',
     callbackURL: 'http://localhost:3000/login/facebook/return'
   },
   function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
     console.log("\n************* token **********************************\n\n");
     console.log(accessToken);
     console.log("\n*************  profile *************************************\n");
@@ -34,16 +23,6 @@ passport.use(new Strategy({
     return cb(null, profile);
   }));
 
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
-// and deserialized.
 passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
@@ -52,28 +31,19 @@ passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
 
-
-// Create a new Express application.
 var app = express();
 
-// Configure view engine to render EJS templates.
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-// Define routes.
 app.get('/',
   function(req, res) {
     res.render('home', { user: req.user });
@@ -100,27 +70,12 @@ app.get('/profile',
   });
 
 /*********************************** TWITER LOGIN ***********************************/
-
-
-// Configure the Twitter strategy for use by Passport.
-//
-// OAuth 1.0-based strategies require a `verify` function which receives the
-// credentials (`token` and `tokenSecret`) for accessing the Twitter API on the
-// user's behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
 passport.use(new StrategyTwitter({
     consumerKey: 'PJnxHFNDKjGQ4Tw4I2Q1RBN3q',
     consumerSecret: 'uIcmi6i7vrqDiT4tQY2XIpAQ3E4XFZU9RmnddR9zervGQ4o1t1',
     callbackURL: 'http://127.0.0.1:3000/login/twitter/return'
   },
   function(token, tokenSecret, profile, cb) {
-    // In this example, the user's Twitter profile is supplied as the user
-    // record.  In a production-quality application, the Twitter profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-
     console.log("\n\n\n************* token **********************************\n\n");
     console.log(token);
     console.log("\n*************  profile **********************************\n");
@@ -129,27 +84,14 @@ passport.use(new StrategyTwitter({
     console.log(tokenSecret);
     console.log("\n*************  name *************************************\n");
     console.log(profile.displayName);
-
  	console.log("\n*************  friends count ******************************\n");
     console.log(profile._json.friends_count);
-
-    	console.log("\n*************  description *****************************\n");
+    console.log("\n*************  description *****************************\n");
     console.log(profile._json.description);
-
     console.log("\n*******************************************************\n\n\n");
     return cb(null, profile);
   }));
 
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Twitter profile is serialized
-// and deserialized.
 passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
@@ -157,10 +99,6 @@ passport.serializeUser(function(user, cb) {
 passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
-
-
-
-// Define routes.
 
 app.get('/login/twitter',
   passport.authenticate('twitter'));
@@ -171,10 +109,36 @@ app.get('/login/twitter/return',
     res.redirect('/');
   });
 
-app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('profile', { user: req.user });
+
+/*************************** google login *********************************/
+
+passport.use(new StrategyGoogle({
+    clientID: '562298891086-88n4c1l2h8vvrhbs18dd0ti7p1pq56ar.apps.googleusercontent.com',
+    clientSecret: 'kx7q822eSxcEW1e4PQBEkJl6',
+    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    
+      return cb(null, profile);
+    }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
   });
 
 app.listen(3000);
