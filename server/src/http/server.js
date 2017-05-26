b6925a0945fd9ecb06811b8e3f56751bc55c4850
jolasman <jolasman@hotmail.com>
2017-05-26 21:58:10 +0100

    twitter login done

--- server/src/http/server.js
+++ server/src/http/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
+var StrategyTwitter = require('passport-twitter').Strategy;
 
 // Configure the Facebook strategy for use by Passport.
 //
@@ -88,6 +89,84 @@ app.get('/login/facebook',
 
 app.get('/login/facebook/return', 
   passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+/*********************************** TWITER LOGIN ***********************************/
+
+
+// Configure the Twitter strategy for use by Passport.
+//
+// OAuth 1.0-based strategies require a `verify` function which receives the
+// credentials (`token` and `tokenSecret`) for accessing the Twitter API on the
+// user's behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new StrategyTwitter({
+    consumerKey: 'PJnxHFNDKjGQ4Tw4I2Q1RBN3q',
+    consumerSecret: 'uIcmi6i7vrqDiT4tQY2XIpAQ3E4XFZU9RmnddR9zervGQ4o1t1',
+    callbackURL: 'http://127.0.0.1:3000/login/twitter/return'
+  },
+  function(token, tokenSecret, profile, cb) {
+    // In this example, the user's Twitter profile is supplied as the user
+    // record.  In a production-quality application, the Twitter profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+
+    console.log("\n\n\n************* token **********************************\n\n");
+    console.log(token);
+    console.log("\n*************  profile **********************************\n");
+    console.log(profile);
+    console.log("\n*************  token secret *****************************\n");
+    console.log(tokenSecret);
+    console.log("\n*************  name *************************************\n");
+    console.log(profile.displayName);
+
+ 	console.log("\n*************  friends count ******************************\n");
+    console.log(profile._json.friends_count);
+
+    	console.log("\n*************  description *****************************\n");
+    console.log(profile._json.description);
+
+    console.log("\n*******************************************************\n\n\n");
+    return cb(null, profile);
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Twitter profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+
+// Define routes.
+
+app.get('/login/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/login/twitter/return', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   });
