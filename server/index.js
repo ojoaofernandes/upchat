e77e482c69a13b6f2a8f84b5e39c0bece32548e9
jolasman <jolasman@hotmail.com>
2017-05-27 19:16:45 +0100

    up

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 'use script';
 
 const tcpServer  = require('./src/tcp/server.js');
-const httpServer = require('./src/http/server.js');
+//const httpServer = require('./src/http/server.js');
 
 tcpServer.run({
     host: 'localhost',
     port: 5570
 });
 
-httpServer.run({
+/*httpServer.run({
     host: 'localhost',
     port: 5580
-});
+});*/
--- server/src/tcp/protocolHandler.js
+++ server/src/tcp/protocolHandler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var net = require('net');
+
 const LOGIN_REQUEST = function (message) {
     if (message.header.params[0] === 'facebook') {
         message.sender.socket.write('url do facebook');
@@ -9,12 +11,45 @@ const LOGIN_REQUEST = function (message) {
     message.sender.socket.write('Invalid LOGIN_REQUEST parameter.');
 }
 
+const LOGIN_SUCCESS = function (message) {
+    if (message.header.protocol === 'LOGIN_SUCCESS') {
+    
+    var client = new net.Socket();
+    client.connect(9090, '127.0.0.1', function() {
+    console.log('Connected');  // acknowledge socket connection
+    client.write(message.header.params[0] + "experiencia"); // send info to Server
+    });
+
+    client.on('close', function() {
+    console.log('Connection closed');
+    });
+    }
+
+    client.on('end',function(){
+    console.log("Reading end");
+    });
+
+    client.on('error', function(err){
+    console.log("Error: "+err.message);
+    })
+
+    console.log("enviou mensagem para : 9090");
+
+    message.sender.socket.write('cenas e coisas');
+}
+
 const handle = function(message, userLists) {
     if (message.sender.status === userLists.status.anonymous) {
         if (message.header.protocol === 'LOGIN_REQUEST') {
             LOGIN_REQUEST(message);
             return;
         }
+
+       else if (message.header.protocol === 'LOGIN_SUCCESS') {
+            LOGIN_SUCCESS(message);
+            return;
+        }
+
 
         message.sender.socket.write('Invalid message.');
         return;
