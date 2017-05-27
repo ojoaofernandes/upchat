ad5929374ec2a362060d89da726304aabde0167b
zabrn <ei10117@fe.up.pt>
2017-05-27 17:07:46 +0100

    adicionados botoes para as plataformas

--- client/java/src/display/Main.java
+++ client/java/src/display/Main.java
@@ -12,6 +12,9 @@ public class Main {
         final JFrame frame = new JFrame("JDialog Demo");
         final JButton btnLogin = new JButton("Click to login");
         final JButton faceLogin = new JButton("Facebook");
+        final JButton twitterLogin = new JButton("Twitter");
+        final JButton googleLogin = new JButton("Google+");
+
         btnLogin.addActionListener(
                 new ActionListener(){
                     public void actionPerformed(ActionEvent e) {
@@ -39,11 +42,42 @@ public class Main {
                     }
                 });
 
+        twitterLogin.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent e) {
+                        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+                        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+                            try {
+                                desktop.browse(new URI("www.twitter.com"));
+                            } catch (Exception exp) {
+                                exp.printStackTrace();
+                            }
+                        }
+                    }
+                });
+
+        googleLogin.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent e) {
+                        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+                        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+                            try {
+                                desktop.browse(new URI("www.google.com"));
+                            } catch (Exception exp) {
+                                exp.printStackTrace();
+                            }
+                        }
+                    }
+                });
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setSize(300, 100);
         frame.setLayout(new FlowLayout());
         frame.getContentPane().add(btnLogin);
         frame.getContentPane().add(faceLogin);
+        frame.getContentPane().add(twitterLogin);
+        frame.getContentPane().add(googleLogin);
         frame.setVisible(true);
     }
 
